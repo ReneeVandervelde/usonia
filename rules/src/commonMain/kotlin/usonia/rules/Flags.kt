2076701351574c99691fc4be0852e5ad71de6205
Renee Vandervelde <Renee@ReneeVandervelde.com>
2022-02-27 17:11:41 -0600

    Disable idle rules when lighting disabled

--- rules/src/commonMain/kotlin/usonia/rules/Flags.kt
+++ rules/src/commonMain/kotlin/usonia/rules/Flags.kt
@@ -5,6 +5,7 @@ import usonia.kotlin.map
 
 object Flags {
     val SleepMode = "Sleep Mode"
+    val DisableMode = "Disable Lights"
 }
 
 val ConfigurationAccess.sleepMode get() = flags.map { it[Flags.SleepMode]?.toBoolean() ?: false }
--- rules/src/commonMain/kotlin/usonia/rules/lights/DisableMode.kt
+++ rules/src/commonMain/kotlin/usonia/rules/lights/DisableMode.kt
@@ -2,9 +2,8 @@ package usonia.rules.lights
 
 import usonia.core.state.getBooleanFlag
 import usonia.foundation.Room
+import usonia.rules.Flags
 import usonia.server.client.BackendClient
-
-private const val FLAG = "Disable Lights"
 
 /**
  * Disable light controls entirely.
@@ -13,7 +12,15 @@ internal class DisableMode(
     private val client: BackendClient,
 ): LightSettingsPicker {
     override suspend fun getActiveSettings(room: Room): LightSettings {
-        if (client.getBooleanFlag(FLAG)) {
+        if (client.getBooleanFlag(Flags.DisableMode)) {
+            return LightSettings.Ignore
+        }
+
+        return LightSettings.Unhandled
+    }
+
+    override suspend fun getIdleSettings(room: Room): LightSettings {
+        if (client.getBooleanFlag(Flags.DisableMode)) {
             return LightSettings.Ignore
         }
 
